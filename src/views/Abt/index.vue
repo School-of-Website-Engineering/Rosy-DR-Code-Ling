fa389e0ad0c767f8e744b2cb3dbaa3798be37801
PLee <101233711+pLeepl@users.noreply.github.com>
2022-11-28 18:58:38 +0800

    关于我们：代码提交

--- src/views/Abt/index.vue
+++ src/views/Abt/index.vue
@@ -1,19 +1,69 @@
 <template>
-    <div>
-asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
+    <div class="why-choose">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 text-center wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="section-head">
+                        <h2 class="title">Why Choose Markpro</h2>
+                        <p class="text">
+                            Anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the
+                            Internet tend to repeat predefined necessary.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-3 col-md-6 wow fadeInUp text-center" data-wow-delay="0.2s">
+                    <div class="chose-box">
+                        <div class="thumb">
+                            <img src="./images/LingASDJ.png" alt="" />
+                        </div>
+                        <h5 class="chose-head">LingASDJ</h5>
+                        <p class="text">
+                            Lorem ipsum dummy text in print and website industry are use in these section
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 wow fadeInUp text-center" data-wow-delay="0.3s">
+                    <div class="chose-box">
+                        <div class="thumb">
+                            <img src="./images/Mason.jpg" alt="" />
+                        </div>
+                        <h5 class="chose-head">Mason</h5>
+                        <p class="text">
+                            Lorem ipsum dummy text in print and website industry are use in these section
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 wow fadeInUp text-center" data-wow-delay="0.4s">
+                    <div class="chose-box">
+                        <div class="thumb">
+                            <img src="./images/liuliu.jpg" alt="" />
+                        </div>
+                        <h5 class="chose-head">liuliu</h5>
+                        <p class="text">
+                            Lorem ipsum dummy text in print and website industry are use in these section
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 wow fadeInUp text-center" data-wow-delay="0.5s">
+                    <div class="chose-box">
+                        <div class="thumb">
+                            <img src="./images/Plee.png" alt="" />
+                        </div>
+                        <h5 class="chose-head">Plee</h5>
+                        <p class="text">
+                            Lorem ipsum dummy text in print and website industry are use in these section
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-div{
-    height: 400px;
-    width: 400px;
-    background-color: rgb(33, 29, 21);
-}
-</style>
+<style lang="scss" scoped></style>
