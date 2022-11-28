0b6da147d8fac9b2d5332b9d5e0571d20894611a
liusxs <1694181697@qq.com>
2022-11-28 20:50:25 +0800

    页面更新

---
+++ src/views/Mods/TML/bane/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="breadcrumb-area">
+    <div class="container">
+        <div class="content">
+            <h2 class="breadd  wow fadeInUp">
+                Product Details
+            </h2>
+            <ul class="breadcrumb-list  wow fadeInUp">
+                <li> <a href="index.html">Home /</a></li>
+                <li>Product Details</li>
+            </ul>
+        </div>
+    </div>
+</section>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Mods/TML/index.vue
+++ src/views/Mods/TML/index.vue
@@ -1,52 +1,17 @@
 <template>
-    <div class="tiany">
-        <div class="tiany-headr">
-            <div class="headr-left">
-                <a href=""><img src="../TML/images/banner.jpg" alt=""></a>
-            </div>
-            <div class="headr-right">
-                <h2>欢迎来到天狱之境</h2>
-                <div class="right-text">
-                    <h5>这是一篇指南页面，能为你的游戏进程提供帮助！</h5>
-                    <h6>天狱之境*开发总群：6666666</h6>
-                </div>
-            </div>
-        </div>
-    </div>
+       <div>
+        <Bane></Bane>
+       </div>
 </template>
 <script>
-    export default {
-        
-    }
+import Bane from '../TML/bane/'
+export default {
+    components: {
+        Bane
+    },
+}
 </script>
 
 <style lang="scss" scoped>
-        .tiany{
-            width: 1500px;
-            margin: 0 auto;
-            .tiany-headr{
-                margin-top: 100px;
-                display: flex;
-                .headr-right{
-                    width: 60%;
-                    background: #29b9f5;
-                    h2{
-                        text-align: center;
-                        padding: 45px;
-                    }
-                    .right-text{
-                        width: 500px;
-                        margin: 0 auto;
-                        margin-top: 15px;
-                        padding: 25px;
-                        text-align: center;
-                        border:  1px solid #7e3afb;
-                        border-radius: 50px;
-                        h5{
-                            padding-bottom: 15px;
-                        }
-                    }
-                }
-            }
-        }
+
 </style>
