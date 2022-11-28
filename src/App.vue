ae86623c7a7efd6a7818dd465c918551f289af03
mason369 <maosenyang00@gmail.com>
2022-11-28 13:55:55 +0800

    improvement:重新引入样式

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,32 @@
     <div id="app">
         <Header></Header>
         <router-view></router-view>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
-import Header from '../src/components/Header/Header'
+import Header from "../src/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 export default {
-    name: 'App',
+    name: "App",
     //注册组件
     components: {
+        Footer,
         Header,
     },
-}
+};
 </script>
 
 <style lang="scss" scoped>
+@import "./assets/css/style.css";
+@import "./assets/css/bootstrap.min.css";
+@import "./assets/css/owl.carousel.min.css";
+@import "./assets/css/magnific-popup.css";
+@import "./assets/css/nice-select.css";
+@import "./assets/css/animate.min.css";
+@import "./assets/css/font-awesome.min.css";
+@import "./assets/css/style.css";
+@import "./assets/css/responsive.css";
 </style>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,79 @@
+<template>
+    <footer class="footer-area two">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="footer-box one">
+                        <div class="logo">
+                            <img src="static/picture/logo-2.png" alt="Markpro" />
+                        </div>
+                        <p class="text">
+                            Popularised in the with the release of etras sheets containing passages and more rcently
+                            with desop publishing software like Maker and all of them including System.more rcently with
+                            desop publishing software like Maker
+                        </p>
+
+                        <div class="social">
+                            <a href="#0"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#0"><i class="fab fa-twitter"></i></a>
+                            <a href="#0"><i class="fab fa-pinterest-p"></i></a>
+                            <a href="#0"><i class="fab fa-google-plus-g"></i></a>
+                            <a href="#0"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="footer-box two">
+                        <h4 class="lasthead">Quick Resources</h4>
+                        <div class="footer-list">
+                            <ul>
+                                <li><a href="#0"> About Us </a></li>
+                                <li><a href="#0"> My account </a></li>
+                                <li class="active"><a href="#0"> Themes </a></li>
+                                <li><a href="#0"> Contact Us </a></li>
+                                <li><a href="#0"> Checkout </a></li>
+                                <li><a href="#0"> Contact Us </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+                    <div class="footer-box three">
+                        <h4 class="lasthead">Markpro Market</h4>
+                        <div class="footer-list">
+                            <ul>
+                                <li><a href="#0"> Terms </a></li>
+                                <li><a href="#0"> Licenses </a></li>
+                                <li><a href="#0"> Market API </a></li>
+                                <li><a href="#0"> Become an affiliate </a></li>
+                                <li><a href="#0"> Privacy & Policy </a></li>
+                                <li><a href="#0"> Site Map </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="footer-box none">
+                        <h4 class="lasthead">Quick Resources</h4>
+                        <div class="footer-list">
+                            <ul>
+                                <li><a href="#0"> Community </a></li>
+                                <li><a href="#0"> Blog </a></li>
+                                <li><a href="#0"> Forums </a></li>
+                                <li><a href="#0"> Meetups </a></li>
+                                <li><a href="#0"> Events </a></li>
+                                <li><a href="#0"> Bloging </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
