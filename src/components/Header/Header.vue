f3510f16412064e66f523939a26d60611b20c1a9
LingASDJ <2735951230@qq.com>
2022-11-28 16:12:18 +0800

    删除JS,改为托管。并且调整头部页面

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,66 +1,115 @@
 <template>
-    <header class="header-two sticky-header">
-        <div class="header-navigation">
-            <div class="container-fluid d-flex align-items-center justify-content-between container-1470">
-                <div class="header-left">
-                    <div class="site-logo">
-                        <a href=""><img alt="Markpro" src="../../components/Header/images/logo.png"></a>
-                    </div>
-                </div>
-                <div class="header-right d-flex align-items-center justify-content-end">
-                    <div class="site-nav-menu">
-                        <ul class="primary-menu">
-                            <li class="current">
-                                <a class="drop" href="#">星河征途(RTS)</a>
-                            </li>
-                            <li>
-                                <a class="drop" href="#">魔绫地牢(PD)</a>
-                            </li>
-                            <li>
-                                <a class="drop" href="#">天狱之境(TML)</a>
-                            </li>
-                            <li>
-                                <a class="drop" href="#">开发团队(Dev)</a>
-                                <ul class="submenu">
-                                    <li><a href="#">Re-重置核心工作室</a></li>
-                                    <li><a href="#">MLPD联合制作工作室</a></li>
-                                    <li><a href="#">SkyHell--开发总部</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a class="drop" href="#">关于我们(Abt)</a>
-                            </li>
-                            <li>
-                                <a class="drop" href="#">网页致谢(Thanks)</a>
-                            </li>
-                        </ul>
-                        <a class="nav-close" href="#0"><i class="fal fa-times"></i></a>
-                    </div>
-                    <!-- <div class="header-extra d-flex align-items-center">
-                        <div class="search-widget">
-                            <a class="search-icon" href="#0"><i class="fal fa-search"></i></a>
-
-                            <div class="search-form">
-                                <form action="#">
-                                    <input placeholder="Type keywords & hit enter" type="search">
-                                </form>
-
-                                <a class="search-close" href="#0"><i class="fal fa-times"></i></a>
-                            </div>
+    <div id="app">
+        <header class="header-two sticky-header">
+            <div class="header-navigation">
+                <div class="container-fluid d-flex align-items-center justify-content-between container-1470">
+                    <div class="header-left">
+                        <div class="site-logo">
+                            <a href=""><img src="../Header/images/logo.png" alt="Markpro"></a>
                         </div>
-                        <div class="offcanvas-widget d-none">
-                            <div class="offcanvas-icon">
+                    </div>
+                    <div class="header-right d-flex align-items-center justify-content-end">
+                        <div class="site-nav-menu">
+                            <ul class="primary-menu">
+                                <li class="current">
+                                    <a class="drop" href="#">网站主页</a>
+                                </li>
+                                <li>
+                                    <a class="drop" href="#">模组列表</a>
+                                    <ul class="submenu">
+                                        <li><a href="#">魔绫地牢-PXD</a></li>
+                                        <li><a href="#">星河征途-RTS</a></li>
+                                        <li><a href="#">天狱之境-TML</a></li>
+                                    </ul>
+                                </li>
+                                <!-- <li>
+                                    <a class="drop" href="#">魔绫地牢(PD)</a>
+                                </li>
+                                <li>
+                                    <a class="drop" href="#">天狱之境(TML)</a>
+                                </li> -->
+                                <li>
+                                    <a class="drop" href="#">开发团队(Dev)</a>
+                                    <ul class="submenu">
+                                        <li><a href="#">Re-重置核心工作室</a></li>
+                                        <li><a href="#">MLPD联合制作工作室</a></li>
+                                        <li><a href="#">SkyHell--开发总部</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a class="drop" href="#">关于我们(Abt)</a>
+                                </li>
+                                <li>
+                                    <a class="drop" href="#">网页致谢(Thanks)</a>
+                                </li>
+                            </ul>
+                            <a class="nav-close" href="#0"><i class="fal fa-times"></i></a>
+                        </div>
+                        <div class="header-extra d-flex align-items-center">
+                            
+                            <!-- <div class="shoping-mini-cart menu-cart">
+                                <a href="#0" class="cart-icon cart-contents menu-item"><i class="fal fa-shopping-cart"></i>
+                                    <span class="number cart-contents-count">2</span>
+                                </a>
+                                <div class="mini-cart">
+                                    <div class="widget woocommerce widget_shopping_cart">
+                                        <div class="widget_shopping_cart_content">
+                                            <ul class="woocommerce-mini-cart cart_list product_list_widget  list-unstyled">
+                                                <li class="woocommerce-mini-cart-item mini_cart_item">
+                                                    <a href="#0" class="remove remove_from_cart_button">×</a>
+                                                    <a href="product-details.html">
+                                                        <img width="750" alt="" height="430" src="static/picture/cart.png" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail">Navian
+                                                    </a>
+                                                    <span class="quantity">1 ×
+                                                        <span class="woocommerce-Price-amount amount">
+                                                            <bdi>
+                                                                <span class="woocommerce-Price-currencySymbol">$</span> 49
+                                                            </bdi>
+                                                        </span>
+                                                    </span>
+                                                </li>
+                                            </ul>
+                                            <p class="woocommerce-mini-cart__total total">
+                                                <strong>Subtotal:</strong> <span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>49</bdi></span>
+                                            </p>
+                                            <p class="woocommerce-mini-cart__buttons buttons">
+                                                <a href="#0" class="button wc-forward main-btn first">View cart</a>
+                                                <a href="checkout.html" class="button checkout wc-forward main-btn">Checkout</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+    
+                            </div>
+     -->
+                            <!-- <div class="search-widget">
+                                <a href="#0" class="search-icon"><i class="fal fa-search"></i></a>
+    
+                                <div class="search-form">
+                                    <form action="#">
+                                        <input type="search" placeholder="Type keywords & hit enter">
+                                    </form>
+    
+                                    <a href="#0" class="search-close"><i class="fal fa-times"></i></a>
+                                </div>
+                            </div> -->
+                            <div class="offcanvas-widget d-none">
+                                <div class="offcanvas-icon">
+                                    <span></span><span></span><span></span>
+                                </div>
+                            </div>
+                            <div class="nav-toggler">
                                 <span></span><span></span><span></span>
                             </div>
+                            <!-- <div class="navbar-btn">
+                                <a href="#0"> <i class="fas fa-user"></i>My Account</a>
+                            </div> -->
                         </div>
-                        <div class="nav-toggler">
-                            <span></span><span></span><span></span>
-                        </div>
-                    </div> -->
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
+    </div>
 </template>
 
 <script>
@@ -70,5 +119,4 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
 </style>
--- src/views/Home/Banner/index.vue
+++ src/views/Home/Banner/index.vue
@@ -5,17 +5,17 @@
                 <div class="col-lg-10 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="banner-content text-center">
                         <h1 class="head">
-                            哈哈哈哈哈哈哈哈哈哈
+                        Hi,欢迎您来到Code-Ling。
                         </h1>
                         <p class="text">
-                           欢迎来到魔ling的世界!
+                            <span id="sitetime"></span>
                         </p>
-                        <form action="#">
+                        <!-- <form action="#">
                             <div class="form-group">
                                 <input type="text" placeholder="输入关键词进行搜索" class="form-control">
                                 <button class="main-btn icon"><i class="fal fa-search"></i></button>
                             </div>
-                        </form>
+                        </form> -->
                     </div>
                     <div class="video">
                         <img src="../Banner/images/laptop.png" alt="" class="laptop">
