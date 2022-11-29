f6018863b838def166c1d29f73e9d07c3acaaeac
LingASDJ <2735951230@qq.com>
2022-11-29 16:50:02 +0800

    更新主页

--- src/views/Home/Banner/index.vue
+++ src/views/Home/Banner/index.vue
@@ -4,10 +4,10 @@
             <div class="row justify-content-center">
                 <div class="col-lg-10 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="banner-content text-center">
-                        <h1 class="head">
+                        <h1 class="head" style="color:#fff">
                         Hi,欢迎您来到Code-Ling。
                         </h1>
-                        <h3 class="head" style="color:#0bffaa">
+                        <h3 class="head" style="color:#fff">
                             <span>Rosy-Code Ling-Page</span><br>
                             <span>星河征途--天狱之境--魔绫地牢总站点</span>
                         </h3>
