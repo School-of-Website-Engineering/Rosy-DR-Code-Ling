907e6dfd0c30e9983781d496ea1e5d07d99f8c21
mason369 <maosenyang00@gmail.com>
2022-11-29 18:28:44 +0800

    update:update

--- src/views/Home/Partner/index.vue
+++ src/views/Home/Partner/index.vue
@@ -1,79 +1,61 @@
 <template>
-    <div class="partner index-2">
+    
+     <div class="partner index-2">
         <div class="container">
             <div class="row">
-                <div class="col-12 wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
-                    <div class="totalpartner owl-carousel owl-theme owl-loaded owl-drag">
-                        <div class="owl-stage-outer">
-                            <div class="owl-stage"
-                                 style="transform: translate3d(-1356px, 0px, 0px); transition: all 1.2s ease 0s; width: 3390px;">
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-1.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-2.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-3.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-4.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-5.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                   class="single item">
-                                    <img src="./images/partner-1.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item active" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-2.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item active" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-3.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item active" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-4.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item active" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-5.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned active" style="width: 206px; margin-right: 20px;"><a
-                                    href="#0" class="single item">
-                                    <img src="./images/partner-1.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-2.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-3.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-4.png" alt="partner">
-                                </a></div>
-                                <div class="owl-item cloned" style="width: 206px; margin-right: 20px;"><a href="#0"
-                                                                                                          class="single item">
-                                    <img src="./images/partner-5.png" alt="partner">
-                                </a></div>
-                            </div>
-                        </div>
-                        <div class="owl-nav disabled">
-                            <div class="owl-prev">prev</div>
-                            <div class="owl-next">next</div>
-                        </div>
-                        <div class="owl-dots disabled"></div>
+                <div class="col-12 wow fadeInUp">
+                    <div class="totalpartner owl-carousel owl-theme">
+                        <a href="#0" class="single item">
+                            <img src="./images/ml.jpg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/re.jpg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/sh4.jpg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/SH.png" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669714974149.gif" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669714986660.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/sh3.jpg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715000657.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715012332.png" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715035574.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/sh2.jpg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715083345.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715089893.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715095499.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715105145.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/1669715113785.jpeg" alt="partner">
+                        </a>
+                        <a href="#0" class="single item">
+                            <img src="./images/sh1.jpg" alt="partner">
+                        </a>
                     </div>
                 </div>
             </div>
@@ -83,53 +65,53 @@
 
 <script>
 export default {
-    mounted() {
-        const s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.src = 'https://jdsalingzx.top/assets/jsx/jquery-3.6.0.min.js';
-        document.body.appendChild(s);
+    mounted () {
+        const s = document.createElement('script')
+        s.type = 'text/javascript'
+        s.src = 'https://jdsalingzx.top/assets/jsx/jquery-3.6.0.min.js'
+        document.body.appendChild(s)
 
-        const a = document.createElement('script');
-        a.type = 'text/javascript';
-        a.src = 'https://jdsalingzx.top/assets/jsx/bootstrap.bundle.min.js';
-        document.body.appendChild(a);
+        const a = document.createElement('script')
+        a.type = 'text/javascript'
+        a.src = 'https://jdsalingzx.top/assets/jsx/bootstrap.bundle.min.js'
+        document.body.appendChild(a)
 
-        const b = document.createElement('script');
-        b.type = 'text/javascript';
-        b.src = 'https://jdsalingzx.top/assets/jsx/owl.carousel.min.js';
-        document.body.appendChild(b);
+        const b = document.createElement('script')
+        b.type = 'text/javascript'
+        b.src = 'https://jdsalingzx.top/assets/jsx/owl.carousel.min.js'
+        document.body.appendChild(b)
 
-        const c = document.createElement('script');
-        c.type = 'text/javascript';
-        c.src = 'https://jdsalingzx.top/assets/jsx/jquery.nice-select.min.js';
-        document.body.appendChild(c);
+        const c = document.createElement('script')
+        c.type = 'text/javascript'
+        c.src = 'https://jdsalingzx.top/assets/jsx/jquery.nice-select.min.js'
+        document.body.appendChild(c)
 
+        const d = document.createElement('script')
+        d.type = 'text/javascript'
+        d.src = 'https://jdsalingzx.top/assets/jsx/jquery.magnific-popup.min.js'
+        document.body.appendChild(d)
 
-        const d = document.createElement('script');
-        d.type = 'text/javascript';
-        d.src = 'https://jdsalingzx.top/assets/jsx/jquery.magnific-popup.min.js';
-        document.body.appendChild(d);
+        const e = document.createElement('script')
+        e.type = 'text/javascript'
+        e.src = 'https://jdsalingzx.top/assets/jsx/wow.min.js'
+        document.body.appendChild(e)
+        const j = document.createElement('script')
+        j.type = 'text/javascript'
+        j.src = 'https://masonosam.top/assets/js/envato-preview.js'
+        document.body.appendChild(j)
 
-        const e = document.createElement('script');
-        e.type = 'text/javascript';
-        e.src = 'https://jdsalingzx.top/assets/jsx/wow.min.js';
-        document.body.appendChild(e);
-        const j = document.createElement('script');
-        j.type = 'text/javascript';
-        j.src = 'https://masonosam.top/assets/js/envato-preview.js';
-        document.body.appendChild(j);
+        const h = document.createElement('script')
+        h.type = 'text/javascript'
+        h.src = 'https://masonosam.top/assets/js/mixitup.min.js'
+        document.body.appendChild(h)
 
-        const h = document.createElement('script');
-        h.type = 'text/javascript';
-        h.src = 'https://masonosam.top/assets/js/mixitup.min.js';
-        document.body.appendChild(h);
-
-        const x = document.createElement('script');
-        x.type = 'text/javascript';
-        x.src = 'https://jdsalingzx.top/assets/jsx/main.js';
-        document.body.appendChild(x);
+        const x = document.createElement('script')
+        x.type = 'text/javascript'
+        x.src = 'https://jdsalingzx.top/assets/jsx/main.js'
+        document.body.appendChild(x)
     },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
