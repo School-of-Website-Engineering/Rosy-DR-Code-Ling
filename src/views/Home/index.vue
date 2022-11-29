69c0328d376ca5ab6c2895eeba60a967aa471321
liusxs <1694181697@qq.com>
2022-11-29 17:08:35 +0800

    删除多余内容

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,9 +3,6 @@
         <Banner></Banner>
         <!-- <Partner></Partner> -->
         <Choose></Choose>
-        <Unique></Unique>
-        <Plan></Plan>
-        <Testomonial></Testomonial>
         <BlogTwo></BlogTwo>
     </div>
 </template>
@@ -14,19 +11,13 @@
 import Banner from '@/views/Home/Banner'
 import Partner from '@/views/Home/Partner'
 import Choose from '@/views/Home/Choose'
-import Unique from '@/views/Home/Unique'
-import Plan from '@/views/Home/Plan'
-import Testomonial from '@/views/Home/Testomonial'
 import BlogTwo from '@/views/Home/BlogTwo'
 export default {
     components: {
         Banner,
         Partner,
         Choose,
-        Unique,
-        Plan,
         BlogTwo,
-        Testomonial,
     },
     mounted() {
         const s = document.createElement('script');
