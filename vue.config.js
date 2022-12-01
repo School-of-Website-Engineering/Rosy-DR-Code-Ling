3408af72b7633d72e7198cf93c2e2c473ac8a2fe
LingASDJ <2735951230@qq.com>
2022-12-01 10:05:29 +0800

    跨域名忽略

--- vue.config.js
+++ vue.config.js
@@ -9,4 +9,10 @@ module.exports = defineConfig({
 module.exports = {
   lintOnSave: false,
   publicPath: '/Rosy-DR-Code-Ling/',
+  devServer: {
+    allowedHosts: [
+      'vps3.pista.top', // 允许访问的域名地址，即花生壳内网穿透的地址
+      '.pista.top'   // .是二级域名的通配符   
+    ],
+  },
 };
